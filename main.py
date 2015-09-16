ab46d88e1bafff640d3c87a149bc1699574a528e
keshav chintamani <keshav.chintamani@gmail.com>
2015-09-16 19:50:23 +0200

    committing some minor changes

--- main.py
+++ main.py
@@ -5,19 +5,28 @@ import time as Time
 import random as Random
 import Queue
 import serial
-import triniBotMotorController
+#import triniBotMotorController
 
 ticker = 0
 encoderQueue = Queue.Queue()
 
-class FakeController:
+class FakeController(threading.Thread):
 
-    def __init__(self, name):
+    def __init__(self, name, q):
+        threading.Thread.__init__(self)
         self.name = name
+        self.q = q
     def Initialize(self, init):
         self.init = init
     def turnOffMotor(self):
         self.turnoff = True
+
+    def run(self):
+         while(True):
+             self.val = self.q.get()
+             print 'Got %s' % self.val
+             Time.sleep(0.01)
+
 
 class MotorThread(threading.Thread):
 
@@ -34,31 +43,34 @@ class MotorThread(threading.Thread):
 
 class EncoderThread(threading.Thread):
 
-    def __init__(self, threadID, name, counter, q):
+    def __init__(self, threadID, name, q):
         threading.Thread.__init__(self)
         self.threadID = threadID
-        self.counter = counter
         self.name = name
         self.q = q
-        self.Serial = serial.Serial('/dev/ttyUSB0', 9600);
+        #self.Serial = serial.Serial('/dev/ttyUSB0', 9600);
         Random.random()
 
     def run(self):
         #Get
         while (True):
-            line = self.Serial.readline()
+            #line = self.Serial.readline()
+            line = 10
             self.q.put(line)
+            print 'Set %s' % line
+            Time.sleep(0.01)
 
 threadLock = threading.Lock()
 
 #Start a worker thread polling on serial interface for encoders from arduino
 eThread = EncoderThread('1', 'encoder-getter', encoderQueue)
-eThread.run()
+eThread.start()
 
 #start a worker thread driving the motor
 motor1 = triniBotMotorController(1)
 m1Thread = MotorThread('2', motor1, encoderQueue)
-m1Thread.run()
+#m1T= FakeController("hello", encoderQueue)
+m1Thread.start()
 
 
 
--- triniBotMotorController.py
+++ triniBotMotorController.py
@@ -8,18 +8,20 @@ import atexit
 import threading
 
 
-class triniBotMotorController:
+class triniBotMotorController(threading.Thread):
 
-    def __init__(self, motorId):
+    def __init__(self, motorId, q):
 
-        #threading.Thread.__init__(self)
+        threading.Thread.__init__(self)
         self.motorId = motorId
         if (self.motorId < 1 or self.motorId > 4):
             print "ID out of range"
             return (False)
 
+
         self.mh = Adafruit_MotorHAT(addr=0x60)
-        #self.q = q
+        self.q = q
+        self.haltRequest == False
 
     def initialize(self, i2cadd):
         print "Starting motor %s" % self.motorId
@@ -45,12 +47,28 @@ class triniBotMotorController:
             return (False)
 
         print("Running at " + repr(speed))
-        self.motor.setSpeed(speed)
+        pwm = 0
+        while(self.haltRequest == True):
+                for x in range(0, 255):
+                    self.motor.setSpeed(x)
+                    wheel_speed = self.q.get()
+                    if wheel_speed > speed
 
     def stopMotor(self, mID):
         print("Attemping to stop " + repr(mID))
         self.motor.run(Adafruit_MotorHAT.RELEASE)
+        self.haltRequest = True
 
     # recommended for auto-disabling motors on shutdown!
     def turnOffMotors(self):
         self.motor.run(Adafruit_MotorHAT.RELEASE)
+
+    def run(self):
+        wheel_speed = self.q.get()
+        speed = 0;
+        #self.setDirection("FORWARD")
+        self.runMotor(100)
+
+
+
+
