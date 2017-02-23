2cefcefa426beae27f803e3cc10f8248d0f4b32e
Keshav Chintamani <keshav.chintamani@gmail.com>
2017-02-23 16:56:57 +0000

    Restructured the repo to be inline with ROS packages and merged loose scripts and packages into a single package called trinibot_core

---
+++ trinibot_core/scripts/tbballtracker.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import threading
+import time as Time
+
+import rospy
+
+from tbros.core.tbVision import BallTracker
+
+ImgWidth = 640
+ImgHeight = 480
+
+from balltracker.msg import ballcoords
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+def callback(value):
+
+    worker_thread = threading.Thread(name = "ballfinder_worker", target = processFrame, args = [value])
+    worker_thread.start()
+    worker_thread.join()
+
+def processFrame(image):
+
+    global pub1, pub2
+    conVerter = CvBridge()
+
+    #Convert from a ROS image to a CV image
+    try:
+        image = conVerter.imgmsg_to_cv2(image, "bgr8")
+    except CvBridgeError as e:
+        print (e)
+
+    result = bT.computeSpheres(image)
+    msgBallCoords = ballcoords()
+    msgModImage = Image()
+    if result == None:
+        return
+    msgBallCoords.color= "RED"
+    msgBallCoords.x = result[0]
+    msgBallCoords.y = result[1]
+    msgBallCoords.radius = result[2]
+
+    # convert from a CV image to a ROS image
+
+    resImage = result[3]
+
+    #resImage = conVerter.cv2_to_imgmsg(resImage , "bgr8")
+    msgModImage = conVerter.cv2_to_imgmsg(resImage , "bgr8")
+
+    rospy.loginfo(msgBallCoords)
+    pub1.publish(msgBallCoords)
+    pub2.publish(msgModImage)
+
+def Init():
+
+    global pub1, pub2
+    rospy.init_node('tbballtracker', anonymous=True)
+    rospy.Subscriber('/cv_camera/image_raw', Image , callback)
+    pub1 = rospy.Publisher('/tbballtracker/coordinates', ballcoords, queue_size=10)
+    pub2 = rospy.Publisher('/tbballtracker/image_result', Image, queue_size=10)
+    while not rospy.is_shutdown():
+        rospy.loginfo("Doing something")
+        Time.sleep(2)
+
+bT = BallTracker(ImgWidth, ImgHeight, "RED")
+
+if __name__ == '__main__':
+    try:
+        Init()
+    except rospy.ROSInterruptException:
+        pass
---
+++ trinibot_core/scripts/tbmotioncontroller.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+import roslib
+roslib.load_manifest('trinibot_core')
+
+import time as Time
+from tbmotorcontroller import TrackedTrinibot
+import random
+
+from geometry_msgs.msg import TwistStamped
+
+#This callback receives command updates from other triniBot nodes
+def command_callback(data):
+    #Process command
+    global robot
+    #This callback receives pose updates from the sensehat
+    if(data.header.frame_id=="LINEAR"):
+        robot.drive_to_distance( data.twist.linear.x)
+    elif(data.header.frame_id=="ANGULAR"):
+        robot.turn_to_angle(data.twist.angular.z)
+    elif(data.header.frame_id=="STOP"):
+        robot.stop()
+
+
+def listener():
+
+    rospy.init_node('motioncontroller_subscriber', anonymous=True)
+    rospy.Subscriber('/tbmotionplanner/TwistStamped', TwistStamped, command_callback)
+
+    rate= rospy.Rate(1)
+    while not rospy.is_shutdown():
+        #If an obstacle is detected
+        rate.sleep()
+
+robot = TrackedTrinibot("/dev/ttyACM0")
+
+if __name__ == '__main__':
+    listener()
+    robot.Stop()
+
