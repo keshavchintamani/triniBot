8b8caaafd2a42ae4aec056c606ef4125ae6162af
Keshav Chintamani <keshav.chintamani@gmail.com>
2018-05-27 18:06:31 +0200

    Commiting what seems to be a fixed orientation for the IMU

--- trinibot_core/scripts/tbsensehat.py
+++ trinibot_core/scripts/tbsensehat.py
@@ -3,7 +3,7 @@
 from sense_hat import SenseHat
 import time as Time
 import rospy
-import tf.transformations
+import tf.transformations as transforms
 from pyquaternion import Quaternion
 from sensor_msgs.msg import Imu, RelativeHumidity, Temperature, MagneticField
 import numpy as np
@@ -32,8 +32,8 @@ def Start():
     humidity_pub = rospy.Publisher("/trinibot_sensors/humidity", RelativeHumidity, queue_size= 10)
     pose_pub = rospy.Publisher("/trinibot_sensors/imu", Imu, queue_size= 10)
     compass_pub = rospy.Publisher("/trinibot_sensors/compass", MagneticField, queue_size=10)
-    
-    r = rospy.Rate(200) # 10hz
+
+    r = rospy.Rate(10) # 10hz
     gyro = Imu()
     temp = Temperature()
     humid = RelativeHumidity()
@@ -48,23 +48,24 @@ def Start():
     acc_covariance = fetch_param("/imu_acc_covariance", [0.000, 0.000, 0.000,
                                     0.000, 0.000, 0.000,
                                     0.000, 0.000, 0.000])
+    q_rot_180 = transforms.quaternion_from_euler(0, 0, -3.142)
+    quat_p180y = Quaternion(axis=[0, 1, 0], angle=3.14159265)
     while not rospy.is_shutdown():
         #get the gyro values
         gyro.header.stamp = rospy.Time.now()
         gyro.header.frame_id="trinibot_imu"
         #convert euler into quaternion
         #print sense.get_orientation_radians()['roll'], sense.get_orientation_radians()['pitch'], sense.get_orientation_radians()['yaw']
-        quat = tf.transformations.quaternion_from_euler(sense.get_orientation_degrees()['roll']*D2R, \
+        quat = transforms.quaternion_from_euler(sense.get_orientation_degrees()['roll']*D2R, \
                                                 sense.get_orientation_degrees()['pitch']*D2R, \
-                                                sense.get_orientation_degrees()['yaw']*D2R) 
-        
-        quat_n90z = Quaternion(axis=[0,0,1], angle=3.14159265/2) 
-        quat= quat_n90z*quat_NED2ENU(quat)
-        quat_p180y = Quaternion(axis=[0, 1, 0], angle=3.14159265)
-        gyro.orientation.x = quat[0][0][0]
-        gyro.orientation.y = quat[0][0][1]
-        gyro.orientation.z = quat[0][0][2]
-        gyro.orientation.w = quat[0][0][3]
+                                                sense.get_orientation_degrees()['yaw']*D2R)
+
+        #quat= q_rot_90*quat_NED2ENU(quat)
+        quat = q_rot_180*quat
+        gyro.orientation.x = quat[0]#quat[0][0][0]
+        gyro.orientation.y = quat[1]#quat[0][0][1]
+        gyro.orientation.z = quat[2]#quat[0][0][2]
+        gyro.orientation.w = quat[3]#quat[0][0][3]
 
         gyro.orientation_covariance = gyro_covariance
         gyro.angular_velocity_covariance = ang_covariance
@@ -84,19 +85,19 @@ def Start():
         gyro.angular_velocity.z = res[2]# +res
 
         #get temperature
-        temp.header.stamp = rospy.Time.now()
-        temp.temperature = sense.get_temperature()
+        #temp.header.stamp = rospy.Time.now()
+        #temp.temperature = sense.get_temperature()
         #get humidity
         humid.header.stamp = rospy.Time.now()
-        humid.relative_humidity = sense.get_humidity()
+        #humid.relative_humidity = sense.get_humidity()
         #get compass
         compass.header.stamp = rospy.Time.now()
         compass.magnetic_field.x = sense.get_compass_raw()['x']
         compass.magnetic_field.y = sense.get_compass_raw()['y']
         compass.magnetic_field.z = sense.get_compass_raw()['z']
 
-        temp_pub.publish(temp)
-        humidity_pub.publish(humid)
+        #temp_pub.publish(temp)
+        #humidity_pub.publish(humid)
         pose_pub.publish(gyro)
         compass_pub.publish(compass)
         r.sleep()
